aa8fc18028c29cc70df5567eb79eea6d3ea3d3a7
Giacomo Grandi <jak.grandi@gmail.com>
2023-03-12 13:19:41 +0100

    trting rule fratelloGermano

--- parenti.pl
+++ parenti.pl
@@ -9,3 +9,4 @@ genitore(chiara, lia).
 nonno(X, Y) :- genitore(X, Z), genitore(Z, Y).
 antenato(X, Y) :- genitore(X, Y).
 antenato(X, Y) :- genitore(Z, Y), antenato(X, Z).
+fratelloGermano(X, Y) :- genitore(PrimoGenitore, X), genitore(PrimoGenitore, Y), genitore(SecondoGenitore, X), genitore(SecondoGenitore, Y).
