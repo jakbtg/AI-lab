5bc5319c7cb19f83103f09483818fa2d57f1d3bd
Giacomo Grandi <jak.grandi@gmail.com>
2023-03-13 14:56:18 +0100

    refactor product base cases

--- liste.pl
+++ liste.pl
@@ -5,6 +5,7 @@ somma([],0). % somma di una lista vuota è 0
 somma([Head|Tail], R) :- somma(Tail, N), R is Head + N.
 
 % prodotto degli elementi di una lista
-prodotto([],0).
+prodotto([],0). % prodotto di una lista vuota è 0
+prodotto([X],X). % prodotto di una lista con un solo elemento è il valore di quel elemento
 
 prodotto([Head|Tail], Res) :- prodotto(Tail, N), Res is Head * N.
