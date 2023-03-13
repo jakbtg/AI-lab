ec1d90b32fa8a09143aae0bcbc58c11d14393ea9
Giacomo Grandi <jak.grandi@gmail.com>
2023-03-13 14:52:55 +0100

    working on product over list elements

--- liste.pl
+++ liste.pl
@@ -2,4 +2,9 @@
 % Fatto base
 somma([],0). % somma di una lista vuota è 0
 % Regola induttiva
-somma([Head|Tail], R) :- somma(Tail, N), R is Head + N. 
+somma([Head|Tail], R) :- somma(Tail, N), R is Head + N.
+
+% prodotto degli elementi di una lista
+prodotto([],0).
+
+prodotto([Head|Tail], Res) :- prodotto(Tail, N), Res is Head * N.
