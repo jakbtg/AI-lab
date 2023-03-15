aed9beab4715074229d8ec4e6a3d28936b8d96b6
Giacomo Grandi <jak.grandi@gmail.com>
2023-03-15 15:45:18 +0100

    refactor and some tries on pinguini

--- pinguini.pl
+++ pinguini.pl
@@ -1,6 +1,8 @@
 uccello(X) :- pinguino(X).
-% per dire che X vola se X è un uccello e non è un pinguino uso la negazione per fallimento.
-% che in sostanza funziona come segue:
+uccello(tux).
+pinguino(tweety).
+pinguino(tux).
+
+% per dire che X vola se X è un uccello e non è un pinguino uso la negazione per fallimento:
 % prova a dimostrare il goal come se non ci fosse la negazione e poi ribalta il risultato.
 vola(X) :- uccello(X), \+ pinguino(X).
-pinguino(tweety).
