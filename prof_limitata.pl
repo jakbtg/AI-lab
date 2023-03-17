200d29ab3761367bcc37ca0dd433515a91fe3c73
Giacomo Grandi <jak.grandi@gmail.com>
2023-03-17 16:31:09 +0100

    added comment

--- prof_limitata.pl
+++ prof_limitata.pl
@@ -1,5 +1,10 @@
+% inserendo il limite manualmente si limita la profondità di ricerca:
+% se limite = 20, prova tutti i cammini di lunghezza massima 20 e basta
+% in questo modo partendo col limite a 1 e incrementandolo di 1 ad ogni iterazione
+% si ottiene la soluzione ottima - questo metodo corrisponde all'iterative deepening.
+
 % creo predicato per fare l'invocazione corretta a risolvi
-prova(Cammino) :- iniziale(S0), risolvi(S0, Cammino, [], 30), write(Cammino).
+prova(Cammino, Limite) :- iniziale(S0), risolvi(S0, Cammino, [], Limite), write(Cammino).
 
 risolvi(S, [], _, _) :- finale(S), !.
 risolvi(S, [Azione|ListaAzioni], Visitati, ProfMax) :-
