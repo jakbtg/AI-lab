87acc57a9da04db21b33b29b688a5cc67d0a3dbc
Giacomo Grandi <jak.grandi@gmail.com>
2023-03-26 00:50:06 +0100

    produce a better output file

--- pozzato/project/asp_results.py
+++ pozzato/project/asp_results.py
@@ -5,37 +5,43 @@ with open('/Users/jak/Documents/Uni/IALab/AI-lab/pozzato/project/asp_out.txt', '
     line = f.readline()
     result = line.split()
     i = 1
-    assegna = []
+    days = []
     for element in result:
         if re.match(r'assegna', element):
-            assegna.append(element)
+            days.append(element)
             # print(f'{i}: {element}')
         i += 1
 
-# total_matches = len(matches)
-# print(f'Total matches: {total_matches}')
+total_days = len(days)
+print(f'Total assegna: {total_days}')
 
-total_assegna = len(assegna)
-print(f'Total assegna: {total_assegna}')
+# create matrix 380x3 (380 matches, 3 columns: day, home team, away team)
+final_calendar = []
+for element in days:
+    day = re.findall(r'\d+', element)
+    day = int(day[0])
+    home_team = re.findall(r'(?<=\().*?(?=\,)', element)
+    home_team = home_team[0].replace('partita(', '')
+    away_team = re.findall(r'(?<=\,).*?(?=\))', element)
+    away_team = away_team[0]
+    final_calendar.append([day, home_team, away_team])
+
+# sort the calendar by day
+final_calendar.sort(key=lambda x: x[0])
+print(final_calendar)
 
 # create an output file 
-columns = ['Giornata', 'Squadra Casa', 'Squadra Ospite']
+headers = ['Giornata', 'Squadra Casa', 'Squadra Ospite']
 with open('/Users/jak/Documents/Uni/IALab/AI-lab/pozzato/project/asp_final_calendar.tsv', 'w') as f:
-    f.write('\t'.join(columns) + '\n')
-    for match in assegna:
-        match = match.replace('assegna(partita(', '')
-        match = match.replace(')', '')
-        match = match.replace(',', '\t')
-        f.write(match + '\n')
+    f.write('\t'.join(headers) + '\n')
+    for element in final_calendar:
+        f.write('\t'.join(map(str, element)) + '\n')
 
 
-
-
-
-# print all fiorentina matches
-fio_matches = []
-for match in assegna:
-    if re.search(r'fiorentina', match):
-        fio_matches.append(match)
-        print(match)
-print(f'Total Fiorentina matches: {len(fio_matches)}')
+# # print all fiorentina matches
+# fio_matches = []
+# for match in days:
+#     if re.search(r'fiorentina', match):
+#         fio_matches.append(match)
+#         print(match)
+# print(f'Total Fiorentina matches: {len(fio_matches)}')
