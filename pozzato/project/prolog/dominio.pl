1d6a06bbb442738caabab3dc9f077fb8b5e774cc
Giacomo Grandi <jak.grandi@gmail.com>
2023-04-27 17:02:42 +0200

    added change stations

--- pozzato/project/prolog/dominio.pl
+++ pozzato/project/prolog/dominio.pl
@@ -44,3 +44,16 @@ linea(lilla, bignami,
 linea(lilla, san_siro_stadio,
     [bignami, ponale, bicocca, ca_granda, istria, marche, zara, isola, garibaldi, monumentale, cenisio, gerusalemme,
     domodossola, tre_torri, portello, lotto, segesta, san_siro_ippodromo, san_siro_stadio]).
+
+
+% Definisco le stazioni di cambio
+% cambio(nome, lista di linee)
+% nome = nome della stazione
+cambio(cadorna, [rossa, verde]).
+cambio(duomo, [rossa, gialla]).
+cambio(loreto, [rossa, verde]).
+cambio(lotto, [rossa, lilla]).
+cambio(garibaldi, [verde, lilla]).
+cambio(centrale, [verde, gialla]).
+cambio(zara, [gialla, lilla]).
+
