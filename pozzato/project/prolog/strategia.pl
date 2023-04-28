9269184eca5b8ac23beadc88c82345786d2e780b
Giacomo Grandi <jak.grandi@gmail.com>
2023-04-28 12:24:37 +0200

    refactor strategy

--- pozzato/project/prolog/strategia.pl
+++ pozzato/project/prolog/strategia.pl
@@ -1,13 +1,13 @@
 % Definisco la strategia di ricerca
 prova(Cammino) :- iniziale(S), risolvi(S, Cammino, []), write(Cammino).
 
-risolvi(S, [], _) :- finale(S), !.
+risolvi(S, [], _) :- goal(S), !.
 
-risolvi(S, [Azione|ListaAzioni], Visitati) :-
-    applicabile(Azione, S),
-    trasforma(Azione, S, SNuovo),
-    \+ member(SNuovo, Visitati),
-    risolvi(SNuovo, ListaAzioni, [S|Visitati]).
+risolvi(Stato, [Azione|ListaAzioni], Visitati) :-
+    applicabile(Azione, Stato),
+    trasforma(Azione, Stato, NuovoStato),
+    \+ member(NuovoStato, Visitati),
+    risolvi(NuovoStato, ListaAzioni, [NuovoStato|Visitati]).
 
 
 
