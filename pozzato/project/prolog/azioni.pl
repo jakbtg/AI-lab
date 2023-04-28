bce0171be9cdffe5e49cfa9988f16b46e0a9b964
Giacomo Grandi <jak.grandi@gmail.com>
2023-04-28 12:25:00 +0200

    refactor actions and transitions and added utilities

--- pozzato/project/prolog/azioni.pl
+++ pozzato/project/prolog/azioni.pl
@@ -1,23 +1,38 @@
 % Definisco le azioni che possono essere applicate in un certo stato
-applicabile(scendi, stazione(S)) :- 
-    stato(sulla_metro).
+applicabile(scendi, stato(_, Posizione, Linea)) :-
+    Posizione = "in_metro".
 
-applicabile(sali, stazione(S)) :-
-    stato(in_stazione).
+applicabile(sali, stato(_, Posizione, Linea)) :-
+    Posizione = "in_stazione".
 
-applicabile(aspetta, stazione(S)) :-
-    stato(sulla_metro),
-    \+ finale(stazione(S)).
+applicabile(aspetta, stato(Stazione, Posizione, Linea)) :-
+    \+ finale(Stazione),
+    Posizione = "in_metro".
 
 
 % Definisco le transizioni
-trasforma(scendi, stazione(S), stazione(S)) :-
-    retract(stato(sulla_metro)),
-    assert(stato(in_stazione)).
+trasforma(scendi, stato(Stazione, Posizione, Linea), stato(Stazione, NuovaPosizione, NuovaLinea)) :-
+    NuovaPosizione = "in_stazione",
+    NuovaLinea = "nessuna".
 
-trasforma(sali, stazione(S), stazione(S)) :-
-    retract(stato(in_stazione)),
-    assert(stato(sulla_metro)).
+trasforma(sali, stato(Stazione, Posizione, Linea), stato(Stazione, NuovaPosizione, NuovaLinea)) :-
+    NuovaPosizione = "in_metro",
+    trovaLinea(Stazione, NuovaLinea).
 
-trasforma(aspetta, stazione(S), stazione(S1)) :-
-    S1 is S + 1.
+trasforma(aspetta, stato(Stazione, Posizione, Linea), stato(NuovaStazione, Posizione, Linea)) :-
+    successiva(Stazione, Linea, NuovaStazione).
+
+
+% Funzione di utilità per trovare la linea di una stazione
+trovaLinea(Stazione, Linea) :- 
+    linea(X, Y, Fermate), member(Stazione, Fermate), Linea = linea(X, Y, Fermate).
+
+% Funzione di utilità per trovare la fermata successiva
+successiva(Stazione, Linea, StazioneSuccessiva) :-
+    \+ finale(Stazione),
+    linea(_, _, Fermate),
+    nth0(Index, Fermate, Stazione),
+    Index1 is Index + 1,
+    nth0(Index1, Fermate, StazioneSuccessiva).
+    
+
