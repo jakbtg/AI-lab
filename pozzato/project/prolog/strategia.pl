36edcd81bbed1086fc90c8434cc8c828fd761e7e
Giacomo Grandi <jak.grandi@gmail.com>
2023-05-02 14:41:18 +0200

    seems working but still needs work

--- pozzato/project/prolog/strategia.pl
+++ pozzato/project/prolog/strategia.pl
@@ -1,14 +1,16 @@
 % Definisco la strategia di ricerca
-prova(Cammino) :- iniziale(S), risolvi(S, Cammino, []), write(Cammino), length(Cammino, L), nl, write(L).
+prova(Cammino) :- iniziale(S), risolvi(S, Cammino, [], 20), write(Cammino), length(Cammino, L), nl, write(L).
 % prova(Cammino) :- iniziale(S), trovaPercorso(S, Cammino, []), write(Cammino).
 
-risolvi(S, [], _) :- goal(S), !.
+risolvi(S, [], _, _) :- goal(S), !.
 
-risolvi(Stato, [Azione|ListaAzioni], Visitati) :-
+risolvi(Stato, [Azione|ListaAzioni], Visitati, ProfMax) :-
+    ProfMax > 0,
     applicabile(Azione, Stato),
     trasforma(Azione, Stato, NuovoStato),
     \+ member(NuovoStato, Visitati),
-    risolvi(NuovoStato, ListaAzioni, [NuovoStato|Visitati]).
+    NuovaProfMax is ProfMax - 1,
+    risolvi(NuovoStato, ListaAzioni, [NuovoStato|Visitati], NuovaProfMax).
 
 
 % prova con trovaPercorso
