c23be8bc1dcb32906c926b61f0c3927f33d47e36
Giacomo Grandi <jak.grandi@gmail.com>
2023-05-02 14:36:26 +0200

    print path length

--- pozzato/project/prolog/strategia.pl
+++ pozzato/project/prolog/strategia.pl
@@ -1,5 +1,5 @@
 % Definisco la strategia di ricerca
-prova(Cammino) :- iniziale(S), risolvi(S, Cammino, []), write(Cammino).
+prova(Cammino) :- iniziale(S), risolvi(S, Cammino, []), write(Cammino), length(Cammino, L), nl, write(L).
 % prova(Cammino) :- iniziale(S), trovaPercorso(S, Cammino, []), write(Cammino).
 
 risolvi(S, [], _) :- goal(S), !.
@@ -8,7 +8,6 @@ risolvi(Stato, [Azione|ListaAzioni], Visitati) :-
     applicabile(Azione, Stato),
     trasforma(Azione, Stato, NuovoStato),
     \+ member(NuovoStato, Visitati),
-    write(NuovoStato), nl,
     risolvi(NuovoStato, ListaAzioni, [NuovoStato|Visitati]).
 
 
