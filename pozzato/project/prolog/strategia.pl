8d3acd90108b2dad069018f429c40e6322c70c7a
Giacomo Grandi <jak.grandi@gmail.com>
2023-05-03 11:37:55 +0200

    implemented iterative deepening

--- pozzato/project/prolog/strategia.pl
+++ pozzato/project/prolog/strategia.pl
@@ -1,7 +1,27 @@
-% Definisco la strategia di ricerca
-prova(Cammino) :- iniziale(S), risolvi(S, Cammino, [], 25), stampaCammino(Cammino), length(Cammino, L), write('Lunghezza cammino: '), write(L).
+% Inizializzazione del programma
+initialize :- retractall(current_depth(_)), assert(current_depth(1)).
 
 % Definisco la strategia di ricerca
+prova(Cammino) :-
+    iniziale(S),
+    current_depth(P),
+    risolvi(S, Cammino, [], P),
+    stampaCammino(Cammino),
+    length(Cammino, L),
+    write('Lunghezza cammino: '), write(L).
+
+% Passo ricorsivo: proseguo incrementando la profondità
+prova(Cammino) :-
+    current_depth(P),
+    P < 50,
+    NuovaProfondita is P + 1,
+    retractall(current_depth(_)),
+    assert(current_depth(NuovaProfondita)),
+    prova(Cammino).
+
+% Definisco la strategia di ricerca
+% prova(Cammino) :- iniziale(S), risolvi(S, Cammino, [], 25), stampaCammino(Cammino), length(Cammino, L), write('Lunghezza cammino: '), write(L).
+
 % Caso base: se sono arrivato allo stato finale, non devo fare niente
 risolvi(S, [], _, _) :- goal(S), !.
 % Caso ricorsivo: se non sono arrivato allo stato finale, continuo a cercare
@@ -18,15 +38,3 @@ stampaCammino([]).
 stampaCammino([Azione|ListaAzioni]) :-
     format('~w ~w ~w ~w \n', Azione),
     stampaCammino(ListaAzioni).
-    
-    
-
-    
-    
-    
-
-
-    
-
-
-
