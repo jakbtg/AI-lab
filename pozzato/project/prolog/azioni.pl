efd0ca336470b3506370089c0552bef129fa6351
Giacomo Grandi <jak.grandi@gmail.com>
2023-05-06 13:38:45 +0200

    trying different approach for heuristic

--- pozzato/project/prolog/azioni.pl
+++ pozzato/project/prolog/azioni.pl
@@ -7,14 +7,18 @@ applicabile(scendi, stato(Stazione, Posizione, _)) :-
     \+ statoIniziale(stato(Stazione, _, _)),
     (finale(Stazione); cambio(Stazione); statoFinale(stato(Stazione, _, _))).
 
+applicabile(aspetta, stato(Stazione, Posizione, _)) :-
+    Posizione == "in_metro",
+    (statoIniziale(stato(Stazione, _, _)); \+ finale(Stazione)).
+
 % AZIONE CON EURISTICA
 % In pratica se non è una stazione di cambio vuol dire che posso proseguire solamente su una linea.
 % Se invece è una stazione di cambio provo se proseguendo su questa linea mi avvicino di più alla stazione finale,
 % altrimenti la scarto.
-applicabile(aspetta, stato(Stazione, Posizione, Linea)) :-
-    Posizione == "in_metro",
-    (statoIniziale(stato(Stazione, _, _)); \+ finale(Stazione)),
-    (\+ cambio(Stazione); euristica(Stazione, Linea)).
+% applicabile(aspetta, stato(Stazione, Posizione, Linea)) :-
+%     Posizione == "in_metro",
+%     (statoIniziale(stato(Stazione, _, _)); \+ finale(Stazione)),
+%     (\+ cambio(Stazione); euristica(Stazione, Linea)).
 
 % Euristica - se è vera vuol dire che la stazione successiva è più vicina alla stazione finale e quindi posso proseguire
 % euristica(Stazione, Linea) :-
@@ -24,47 +28,66 @@ applicabile(aspetta, stato(Stazione, Posizione, Linea)) :-
 %     manhattan(StazioneSuccessiva, StazioneFinale, D2),
 %     D2 =< D1.
 
-euristica(Stazione) :-
+
+euristica(Stazione, ListaLinee) :-
     findall(Linea, trovaLinea(Stazione, Linea), Linee),
-    write(Linee), nl,
     generaStazioniSuccessive(Stazione, Linee, StazioniSuccessive),
-    write(StazioniSuccessive), nl,
     trovaStazioneFinale(StazioneFinale),
-    write(StazioneFinale), nl,
     generaDistanze(StazioniSuccessive, StazioneFinale, Distanze),
-    write(Distanze), nl,
     associaDistanze(Linee, Distanze, Associazioni),
-    write(Associazioni), nl.
-    
+    ordinaLinee(Associazioni, LineeOrdinate),
+    ListaLinee = LineeOrdinate.
     
 generaStazioniSuccessive(_, [], []).
 generaStazioniSuccessive(Stazione, [Linea|AltreLinee], [StazioneSuccessiva|AltreStazioni]) :-
     successiva(Stazione, Linea, StazioneSuccessiva),
     generaStazioniSuccessive(Stazione, AltreLinee, AltreStazioni).
-
+    
 generaDistanze([], _, []).
 generaDistanze([StazioneSuccessiva|AltreStazioni], StazioneFinale, [Distanza|AltreDistanze]) :-
     manhattan(StazioneSuccessiva, StazioneFinale, Distanza),
     generaDistanze(AltreStazioni, StazioneFinale, AltreDistanze).
-
+    
 associaDistanze([], [], []).
 associaDistanze([Linea|AltreLinee], [Distanza|AltreDistanze], [Associazione|AltreAssociazioni]) :-
     Associazione = (Linea, Distanza),
     associaDistanze(AltreLinee, AltreDistanze, AltreAssociazioni).
-
     
-
-% AZIONE SENZA EURISTICA
-% applicabile(aspetta, stato(Stazione, Posizione, _)) :-
-%     Posizione == "in_metro",
-%     (statoIniziale(stato(Stazione, _, _)); \+ finale(Stazione)).
+    % trovaLineaMigliore([], _, _).
+trovaLineaMigliore([(Linea, Distanza)], LineaMigliore, DistanzaMigliore) :-
+    LineaMigliore = Linea,
+    DistanzaMigliore = Distanza.
+trovaLineaMigliore([(Linea, Distanza)|AltreAssociazioni], LineaMigliore, DistanzaMigliore) :-
+    trovaLineaMigliore(AltreAssociazioni, AltraLinea, AltraDistanza),
+    (Distanza < AltraDistanza -> LineaMigliore = Linea, DistanzaMigliore = Distanza;
+    LineaMigliore = AltraLinea, DistanzaMigliore = AltraDistanza).
     
+ordinaLinee([], []).
+ordinaLinee(Associazioni, LineeOrdinate) :-
+    length(Associazioni, L),
+    L > 0,
+    trovaLineaMigliore(Associazioni, LineaMigliore, DistanzaMigliore),
+    append([LineaMigliore], LineeOrdinateTemp, LineeOrdinate),
+    delete(Associazioni, (LineaMigliore, DistanzaMigliore), AssociazioniRimanenti),
+    ordinaLinee(AssociazioniRimanenti, LineeOrdinateTemp).
+    
+% provaLinea([], _).
+% provaLinea([Linea|AltreLinee], Out) :-
+%     Out = Linea,
+%     provaLinea(AltreLinee, AltroOut).
+
 
 
 %%%%%%%%%% TRASFORMAZIONI DI STATO %%%%%%%%%%
+% trasforma(sali, stato(Stazione, _, _), stato(Stazione, NuovaPosizione, NuovaLinea), Azione) :-
+%     NuovaPosizione = "in_metro",
+%     trovaLinea(Stazione, NuovaLinea),
+%     Azione = ["Sali sulla linea", NuovaLinea, "alla fermata", Stazione].
+
 trasforma(sali, stato(Stazione, _, _), stato(Stazione, NuovaPosizione, NuovaLinea), Azione) :-
     NuovaPosizione = "in_metro",
-    trovaLinea(Stazione, NuovaLinea),
+    euristica(Stazione, Linee),
+    member(NuovaLinea, Linee),
     Azione = ["Sali sulla linea", NuovaLinea, "alla fermata", Stazione].
 
 trasforma(aspetta, stato(Stazione, Posizione, Linea), stato(NuovaStazione, Posizione, Linea), Azione) :-
