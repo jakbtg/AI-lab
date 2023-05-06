036b872ce780764ad3afaf9c00f44f2b017cc9d2
Giacomo Grandi <jak.grandi@gmail.com>
2023-05-06 19:55:09 +0200

    cleaning and refactoring

--- pozzato/project/prolog/azioni.pl
+++ pozzato/project/prolog/azioni.pl
@@ -11,7 +11,9 @@ applicabile(aspetta, stato(Stazione, Posizione, _)) :-
     Posizione == "in_metro",
     (statoIniziale(stato(Stazione, _, _)); \+ finale(Stazione)).
 
-% AZIONE CON EURISTICA
+
+
+% AZIONE CON EURISTICA --- OLD VERSION
 % In pratica se non è una stazione di cambio vuol dire che posso proseguire solamente su una linea.
 % Se invece è una stazione di cambio provo se proseguendo su questa linea mi avvicino di più alla stazione finale,
 % altrimenti la scarto.
@@ -19,7 +21,6 @@ applicabile(aspetta, stato(Stazione, Posizione, _)) :-
 %     Posizione == "in_metro",
 %     (statoIniziale(stato(Stazione, _, _)); \+ finale(Stazione)),
 %     (\+ cambio(Stazione); euristica(Stazione, Linea)).
-
 % Euristica - se è vera vuol dire che la stazione successiva è più vicina alla stazione finale e quindi posso proseguire
 % euristica(Stazione, Linea) :-
 %     trovaStazioneFinale(StazioneFinale),
@@ -27,59 +28,6 @@ applicabile(aspetta, stato(Stazione, Posizione, _)) :-
 %     manhattan(Stazione, StazioneFinale, D1),
 %     manhattan(StazioneSuccessiva, StazioneFinale, D2),
 %     D2 =< D1.
-
-
-euristica(Stazione, ListaLinee) :-
-    findall(Linea, trovaLinea(Stazione, Linea), Linee),
-    generaStazioniSuccessive(Stazione, Linee, StazioniSuccessive),
-    trovaStazioneFinale(StazioneFinale),
-    generaDistanze(StazioniSuccessive, StazioneFinale, Distanze),
-    associaDistanze(Linee, Distanze, Associazioni),
-    ordinaLinee(Associazioni, LineeOrdinate),
-    ListaLinee = LineeOrdinate.
-    
-generaStazioniSuccessive(_, [], []).
-generaStazioniSuccessive(Stazione, [Linea|AltreLinee], [StazioneSuccessiva|AltreStazioni]) :-
-    successiva(Stazione, Linea, StazioneSuccessiva),
-    generaStazioniSuccessive(Stazione, AltreLinee, AltreStazioni).
-
-% rimuoviDoppioni(_, [], []).
-% rimuoviDoppioni(Stazione, ListaStazioniSuccessive, StazioniRimanenti) :-
-%     delete(ListaStazioniSuccessive, Stazione, ListaStazioniSuccessiveTemp),
-
-    
-generaDistanze([], _, []).
-generaDistanze([StazioneSuccessiva|AltreStazioni], StazioneFinale, [Distanza|AltreDistanze]) :-
-    manhattan(StazioneSuccessiva, StazioneFinale, Distanza),
-    generaDistanze(AltreStazioni, StazioneFinale, AltreDistanze).
-    
-associaDistanze([], [], []).
-associaDistanze([Linea|AltreLinee], [Distanza|AltreDistanze], [Associazione|AltreAssociazioni]) :-
-    Associazione = (Linea, Distanza),
-    associaDistanze(AltreLinee, AltreDistanze, AltreAssociazioni).
-    
-% trovaLineaMigliore([], _, _).
-trovaLineaMigliore([(Linea, Distanza)], LineaMigliore, DistanzaMigliore) :-
-    LineaMigliore = Linea,
-    DistanzaMigliore = Distanza.
-trovaLineaMigliore([(Linea, Distanza)|AltreAssociazioni], LineaMigliore, DistanzaMigliore) :-
-    trovaLineaMigliore(AltreAssociazioni, AltraLinea, AltraDistanza),
-    (Distanza < AltraDistanza -> LineaMigliore = Linea, DistanzaMigliore = Distanza;
-    LineaMigliore = AltraLinea, DistanzaMigliore = AltraDistanza).
-    
-ordinaLinee([], []).
-ordinaLinee(Associazioni, LineeOrdinate) :-
-    length(Associazioni, L),
-    L > 0,
-    trovaLineaMigliore(Associazioni, LineaMigliore, DistanzaMigliore),
-    append([LineaMigliore], LineeOrdinateTemp, LineeOrdinate),
-    delete(Associazioni, (LineaMigliore, DistanzaMigliore), AssociazioniRimanenti),
-    ordinaLinee(AssociazioniRimanenti, LineeOrdinateTemp).
-    
-% provaLinea([], _).
-% provaLinea([Linea|AltreLinee], Out) :-
-%     Out = Linea,
-%     provaLinea(AltreLinee, AltroOut).
 
 
 
@@ -106,6 +54,58 @@ trasforma(scendi, stato(Stazione, _, Linea), stato(Stazione, NuovaPosizione, Nuo
 
 
 
+%%%%%%%%%% EURISTICA %%%%%%%%%%
+% Restituisce la lista delle linee disponibili ordinate in base alla distanza di manhattan della ipotetica 
+% stazione successiva della linea corrispondente alla stazione finale.
+% Viene applicata con l'azione sali, in modo da provare a salire sulla line più promettente.
+euristica(Stazione, ListaLinee) :-
+    findall(Linea, trovaLinea(Stazione, Linea), Linee),
+    generaStazioniSuccessive(Stazione, Linee, StazioniSuccessive),
+    trovaStazioneFinale(StazioneFinale),
+    generaDistanze(StazioniSuccessive, StazioneFinale, Distanze),
+    associaDistanze(Linee, Distanze, Associazioni),
+    ordinaLinee(Associazioni, LineeOrdinate),
+    ListaLinee = LineeOrdinate.
+
+% Genera le stazioni successive di una lista di linee   
+generaStazioniSuccessive(_, [], []).
+generaStazioniSuccessive(Stazione, [Linea|AltreLinee], [StazioneSuccessiva|AltreStazioni]) :-
+    successiva(Stazione, Linea, StazioneSuccessiva),
+    generaStazioniSuccessive(Stazione, AltreLinee, AltreStazioni).
+
+% Calcola la distanza di manhattan tra la lista di stazioni successive e la stazione finale
+generaDistanze([], _, []).
+generaDistanze([StazioneSuccessiva|AltreStazioni], StazioneFinale, [Distanza|AltreDistanze]) :-
+    manhattan(StazioneSuccessiva, StazioneFinale, Distanza),
+    generaDistanze(AltreStazioni, StazioneFinale, AltreDistanze).
+
+% Associa le distanze alle linee 
+associaDistanze([], [], []).
+associaDistanze([Linea|AltreLinee], [Distanza|AltreDistanze], [Associazione|AltreAssociazioni]) :-
+    Associazione = (Linea, Distanza),
+    associaDistanze(AltreLinee, AltreDistanze, AltreAssociazioni).
+
+% Trova la linea migliore in base alla distanza di manhattan dalla stazione finale
+trovaLineaMigliore([(Linea, Distanza)], LineaMigliore, DistanzaMigliore) :-
+    LineaMigliore = Linea,
+    DistanzaMigliore = Distanza.
+trovaLineaMigliore([(Linea, Distanza)|AltreAssociazioni], LineaMigliore, DistanzaMigliore) :-
+    trovaLineaMigliore(AltreAssociazioni, AltraLinea, AltraDistanza),
+    (Distanza < AltraDistanza -> LineaMigliore = Linea, DistanzaMigliore = Distanza;
+    LineaMigliore = AltraLinea, DistanzaMigliore = AltraDistanza).
+    
+% Ordina le linee in base alla distanza di manhattan dalla stazione finale
+ordinaLinee([], []).
+ordinaLinee(Associazioni, LineeOrdinate) :-
+    length(Associazioni, L),
+    L > 0,
+    trovaLineaMigliore(Associazioni, LineaMigliore, DistanzaMigliore),
+    append([LineaMigliore], LineeOrdinateTemp, LineeOrdinate),
+    delete(Associazioni, (LineaMigliore, DistanzaMigliore), AssociazioniRimanenti),
+    ordinaLinee(AssociazioniRimanenti, LineeOrdinateTemp).
+
+
+
 %%%%%%%%%%% FUNZIONI DI UTILITÀ %%%%%%%%%%%
 % Trova le linee di una stazione
 trovaLinea(Stazione, Linea) :- 
@@ -119,7 +119,6 @@ successiva(Stazione, Linea, StazioneSuccessiva) :-
     length(Fermate, L),
     (Index1 < L, nth0(Index1, Fermate, StazioneSuccessiva); 
     StazioneSuccessiva = Stazione).
-    
 
 % Trova la lista di fermate di una linea
 fermateLinea(Linea, Fermate) :- 
@@ -134,13 +133,3 @@ manhattan(Stazione1, Stazione2, Distanza) :-
 % Trova la stazione dello stato finale
 trovaStazioneFinale(Stazione) :-
     statoFinale(stato(Stazione, _, _)).
-
-% Itera su lista di linee
-iteraLinee([], _).
-iteraLinee([Linea], Out) :-
-    Out = Linea,
-    write(Out).
-iteraLinee([Linea|AltreLinee], Out) :-
-    Out = Linea,
-    write(Out),
-    iteraLinee(AltreLinee, Out).
