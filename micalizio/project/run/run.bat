9eb53bc799d2c827382e0ba40df7c46146bc4485
Giacomo Grandi <jak.grandi@gmail.com>
2023-05-19 16:21:27 +0200

    watch activations on batch file

--- micalizio/project/run/run.bat
+++ micalizio/project/run/run.bat
@@ -1,5 +1,6 @@
 (load /Users/jak/Documents/Uni/IALab/AI-lab/micalizio/project/0_Main.clp)
 (load /Users/jak/Documents/Uni/IALab/AI-lab/micalizio/project/1_Env.clp)
-(load /Users/jak/Documents/Uni/IALab/AI-lab/micalizio/project/case1_obs_2.clp)
+(load /Users/jak/Documents/Uni/IALab/AI-lab/micalizio/project/maps/2_map1_4obs.clp)
 (load /Users/jak/Documents/Uni/IALab/AI-lab/micalizio/project/3_Agent.clp)
 (reset)
+(watch activations)
