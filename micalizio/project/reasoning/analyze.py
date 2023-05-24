86fe65e5a1b7dce87658ff039ad0b5d65cf1d95e
Giacomo Grandi <jak.grandi@gmail.com>
2023-05-24 17:47:37 +0200

    refactor analyze result

--- micalizio/project/reasoning/analyze.py
+++ micalizio/project/reasoning/analyze.py
@@ -4,7 +4,21 @@ from prettytable import PrettyTable
 with open ('micalizio/project/reasoning/result.txt', 'r') as f:
     lines = f.readlines()
 
-# print(lines)
+# find total pieces per column and row
+pieces_per_row = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+pieces_per_column = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+for line in lines:
+    if re.search(r'know that column', line, re.IGNORECASE):
+        pieces_per_column[int(re.search(r'column (\d)', line).group(1))] = int(re.search(r'(\d) pieces', line).group(1))
+    if re.search(r'know that row', line, re.IGNORECASE):
+        pieces_per_row[int(re.search(r'row (\d)', line).group(1))] = int(re.search(r'(\d) pieces', line).group(1))
+
+print(f'Initial pieces per row: {pieces_per_row}')
+print(f'Initial pieces per column: {pieces_per_column}')
+
+# empty cells per row and column
+empty_cells_per_row = {0: 10, 1: 10, 2: 10, 3: 10, 4: 10, 5: 10, 6: 10, 7: 10, 8: 10, 9: 10}
+empty_cells_per_column = {0: 10, 1: 10, 2: 10, 3: 10, 4: 10, 5: 10, 6: 10, 7: 10, 8: 10, 9: 10}
 
 # find total number of guesses
 total_guesses = 0
@@ -36,6 +50,10 @@ for line in lines:
         x = int(row)
         y = int(column)
         grid[x][y] = '⛵️'
+        pieces_per_row[x] -= 1
+        pieces_per_column[y] -= 1
+        empty_cells_per_row[x] -= 1
+        empty_cells_per_column[y] -= 1
 
 # for line in lines:
     if re.search(r'water', line, re.IGNORECASE) and re.search(r'fill', line, re.IGNORECASE):
@@ -46,6 +64,8 @@ for line in lines:
         x = int(row)
         y = int(column)
         grid[x][y] = '🌊'
+        empty_cells_per_row[x] -= 1
+        empty_cells_per_column[y] -= 1
 
 # check number of sunk boats
 sunk_one = 0
@@ -70,12 +90,14 @@ print(f'Sunk four-pieces: {sunk_four}')
 
 # print the grid with pretty table
 table = PrettyTable()
-table.field_names = [' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-for i in range(10):
-    table.add_row([i, grid[i][0], grid[i][1], grid[i][2], grid[i][3], grid[i][4], grid[i][5], grid[i][6], grid[i][7], grid[i][8], grid[i][9]])
-
-
+table.field_names = [' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Pieces left', 'Empty cells']
+for i in range(9):
+    table.add_row([i, grid[i][0], grid[i][1], grid[i][2], grid[i][3], grid[i][4], grid[i][5], grid[i][6], grid[i][7], grid[i][8], grid[i][9], pieces_per_row[i], empty_cells_per_row[i]])
+table.add_row([9, grid[9][0], grid[9][1], grid[9][2], grid[9][3], grid[9][4], grid[9][5], grid[9][6], grid[9][7], grid[9][8], grid[9][9], pieces_per_row[9], empty_cells_per_row[9]], divider=True)
+table.add_row(['Pieces left', pieces_per_column[0], pieces_per_column[1], pieces_per_column[2], pieces_per_column[3], pieces_per_column[4], pieces_per_column[5], pieces_per_column[6], pieces_per_column[7], pieces_per_column[8], pieces_per_column[9], '', ''])
+table.add_row(['Empty cells', empty_cells_per_column[0], empty_cells_per_column[1], empty_cells_per_column[2], empty_cells_per_column[3], empty_cells_per_column[4], empty_cells_per_column[5], empty_cells_per_column[6], empty_cells_per_column[7], empty_cells_per_column[8], empty_cells_per_column[9], '', ''])
 print(table)
+
 
 
 
