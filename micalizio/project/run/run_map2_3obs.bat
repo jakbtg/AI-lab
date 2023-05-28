5246b01d6317a9d60491d91c9057933813f0a97e
Giacomo Grandi <jak.grandi@gmail.com>
2023-05-28 12:16:18 +0200

    added map 2 with different obs

---
+++ micalizio/project/run/run_map2_3obs.bat
@@ -0,0 +1,6 @@
+(load /Users/jak/Documents/Uni/IALab/AI-lab/micalizio/project/0_Main.clp)
+(load /Users/jak/Documents/Uni/IALab/AI-lab/micalizio/project/1_Env.clp)
+(load /Users/jak/Documents/Uni/IALab/AI-lab/micalizio/project/maps/2_map2_3obs.clp)
+(load /Users/jak/Documents/Uni/IALab/AI-lab/micalizio/project/3_Agent.clp)
+(reset)
+(run)
